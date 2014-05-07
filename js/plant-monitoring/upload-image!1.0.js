51f970f72da2468c85c0be3238c162250e855377
tsuyoshi yano <tyano@yourinventit.com>
2014-05-07 20:28:58 +0900

    add js app

---
+++ js/plant-monitoring/upload-image!1.0.js
@@ -0,0 +1,50 @@
+/*
+ * JobServiceID:
+ * urn:moat:${APPID}:plant-monitoring:upload-image:1.0
+ * 
+ * Description: Notify captured image to the Web UI application.
+ */
+var moat = require('moat');
+var context = moat.init();
+var session = context.session;
+var clientRequest = context.clientRequest;
+var logTag = 'upload-image'
+
+var WEBAPI_IMAGE = 'https://plant-monitoring-test.herokuapp.com/images/';
+
+session.log(logTag, '--- Image arrived ---');
+
+var objects = clientRequest.objects;
+if (objects.length != 1) {
+  throw '*** ERROR!! *** invalid object length:' + objects.length;
+}
+if (objects[0].array.length != 1) {
+  throw '*** ERROR!! *** invalid array length:' + objects[0].array.length;
+}
+notifyImage(clientRequest.device.deviceId, clientRequest.objects[0].array[0]);
+
+function notifyImage(deviceId, entity) {
+  session.fetchUrlSync(
+    WEBAPI_IMAGE,
+    {
+      method : 'POST',
+      contentType : 'application/json',
+      payload : {
+        deviceId : deviceId,
+        uid : entity.uid,
+        timestamp : entity.timestamp,
+        contentType : entity.contentType,
+        encodedContent : entity.encodedContent
+      }
+    },
+    function(response) {
+      if (parseInt(response.responseCode / 100) == 2) {
+        session.log(logTag, 'Notified');
+      } else {
+        session.log(logTag, '*** ERROR!! *** resp code: ' + response.responseCode);
+        throw '*** ERROR!! *** resp code: ' + response.responseCode;
+      }
+    }
+  );
+}
+
---
+++ js/plant-monitoring/upload-sensing-data!1.0.js
@@ -0,0 +1,62 @@
+/*
+ * JobServiceID:
+ * urn:moat:${APPID}:plant-monitoring:upload-sensing-data:1.0
+ * 
+ * Description: Notify captured image to the Web UI application.
+ */
+var moat = require('moat');
+var context = moat.init();
+var session = context.session;
+var clientRequest = context.clientRequest;
+var logTag = 'upload-sensing-data'
+
+var WEBAPI_SENSING_DATA = 'https://plant-monitoring-test.herokuapp.com/sensing-data/';
+
+session.log(logTag, '--- Data arrived ---');
+
+var objects = clientRequest.objects;
+if (objects.length != 1) {
+  throw '*** ERROR!! *** invalid object length:' + objects.length;
+}
+
+var array = rawDataToNotifyData(objects[0].array);
+notifyData(clientRequest.device.deviceId, array);
+
+function rawDataToNotifyData(rawArray) {
+  var  = array[];
+  for (var i = 0; i < rawArray.length; i++) {
+    var e = rawArray[i];
+    var entity = {
+      uid : e.uid,
+      serialNumber : e.serialNumber,
+      temperature : e.temperature,
+      humidity : e.humidity,
+      moisture : e.moisture,
+      timestamp : e.timestamp
+    };
+    array.push(entity);
+  }
+  return array;
+}
+function notifyData(deviceId, rawData) {
+  session.fetchUrlSync(
+    WEBAPI_SENSING_DATA,
+    {
+      method : 'POST',
+      contentType : 'application/json',
+      payload : {
+        deviceId : deviceId,
+        array: array
+      }
+    },
+    function(response) {
+      if (parseInt(response.responseCode / 100) == 2) {
+        session.log(logTag, 'Notified');
+      } else {
+        session.log(logTag, '*** ERROR!! *** resp code: ' + response.responseCode);
+        throw '*** ERROR!! *** resp code: ' + response.responseCode;
+      }
+    }
+  );
+}
+
