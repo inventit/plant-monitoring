0ca0eccf61066763d458da71a1635b915dfb1ec9
tyano@yourinventit.com <tyano@yourinventit.com>
2014-05-07 20:27:52 +0900

    git clone git@heroku.com:plant-monitoring-test.git

---
+++ plant-monitoring-test/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PlantMonitoringTest::Application.config.secret_token = 'c74ce5f8660fac4c6ec5ac873c594e0cf84817eb3178d2cd3a2083458416bcce3bd42da99176235e2b2ace67da1623bcc70d8330b459ce87e22825f371a3fb39'
